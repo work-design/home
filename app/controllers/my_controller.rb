d37ac6baf366984b94a2c613150c4ebe90ff69b1
qinmingyuan <mingyuan0715@foxmail.com>
2019-07-20 16:24:30 +0800

    update gemfile

--- app/controllers/my_controller.rb
+++
@@ -1,5 +0,0 @@
-class MyController < ApplicationController
-  include RailsAuth::Wechat
-  before_action :require_login
-  
-end
--- app/controllers/panel_controller.rb
+++
@@ -1,6 +0,0 @@
-class PanelController < ApplicationController
-  layout 'admin'
-  before_action :require_login
-  after_action :set_flash, only: [:update, :create, :destroy]
-
-end
