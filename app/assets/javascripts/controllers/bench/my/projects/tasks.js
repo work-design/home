4db1a07cb48e603dad73128d9a6cc8b4288325b2
qinmingyuan <mingyuan0715@foxmail.com>
2019-08-02 14:53:49 +0800

    fix side css

--- app/assets/javascripts/controllers/bench/my/projects/tasks.js
+++
@@ -1,4 +0,0 @@
-//= require ../tasks/_sortable
-//= require ../tasks/_show_item
-//= require valid
-//= require_self
--- app/assets/javascripts/controllers/bench/my/tasks/show.js
+++
@@ -1,5 +0,0 @@
-//= require ./_sortable
-//= require ./_show_item
-//= require valid
-
-$('#task_pipeline_id').dropdown();
