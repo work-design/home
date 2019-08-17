55a6866ab20d70186c251fa9a518004980314883
qinmingyuan <mingyuan0715@foxmail.com>
2019-08-17 23:17:43 +0800

    gemfile

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,6 @@ class User < ApplicationRecord
   include RailsRole::User
   include RailsAuth::User
   include RailsTrade::User
-  include RailsTrade::Buyer
   include RailsNotice::Receiver
   include RailsOrg::User
   include RailsProfile::User
