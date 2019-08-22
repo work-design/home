bc70bab8bc0662b27c0ccb840b1e309c271d27cd
qinmingyuan <mingyuan0715@foxmail.com>
2019-08-22 21:59:23 +0800

    webpack compile all js

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -1,7 +1,9 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 
 const environment = require('./environment')
+const test = require('rails_com')
+const env = environment.toWebpackConfig()
+env.entry = Object.assign(test.paths(), env.entry)
+env.resolve.modules = env.resolve.modules.concat(test.roots)
 
-module.exports = environment.toWebpackConfig()
-
-debugger;
+module.exports = env
