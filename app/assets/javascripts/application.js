17a606507c96beac155cd2dcafffdbad85f2fe1f
qinmingyuan <mingyuan0715@foxmail.com>
2019-08-23 14:30:53 +0800

    upgrade semantic ui

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,12 +1,10 @@
-//= require rails_com/application
 //= require rails_com/stimulus
-
-//= require work-ui/dist/semantic
 //= require channels
 
 //= require default_form/valid
 //= require default_form/ready
 //= require_self
 
-require('@rails/ujs').start()
-require('turbolinks').start()
+require('rails_com/application')
+require('jquery')
+require('fomantic-ui')
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,8 +1,11 @@
 const { environment } = require('@rails/webpacker')
 const test = require('rails_com')
 
-const expose = require('./loaders/expose')
-environment.loaders.prepend('expose', expose)
+const jquery = require('rails_com/package/loaders/jquery')
+const rails_ujs = require('rails_com/package/loaders/rails_ujs')
+
+environment.loaders.prepend('jquery', jquery)
+environment.loaders.prepend('rails_ujs', rails_ujs)
 
 const env = environment.toWebpackConfig()
 env.entry = Object.assign(test.paths(), env.entry)
