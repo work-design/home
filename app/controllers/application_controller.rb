66aba50d59085699bec289864676e417c3b13e52
qinmingyuan <mingyuan0715@foxmail.com>
2019-09-04 21:57:59 +0800

    upgrade css

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   include RailsCom::Application
   include RailsRole::Application
   include RailsOrg::Application
+  include RailsTrade::MyCart
   protect_from_forgery with: :exception
   content_security_policy false
 
