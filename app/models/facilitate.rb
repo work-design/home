8e2c86d42a4658050db57f6f93bf7c316e3e15a4
qinmingyuan <mingyuan0715@foxmail.com>
2019-11-29 23:40:43 +0800

    taxon

--- app/models/facilitate.rb
+++ app/models/facilitate.rb
@@ -5,15 +5,18 @@ class Facilitate < ApplicationRecord
   attribute :quantity, :integer, default: 1
   attribute :unified_quantity, :integer, default: 1
   attribute :unit, :string, default: '个'
+  attribute :description, :string
+  attribute :qr_prefix, :string
+  attribute :published, :boolean, default: true
 
   belongs_to :facilitate_taxon, autosave: true, counter_cache: true
+  
   has_one :good_provider, -> { where(selected: true) }, as: :good
   has_one :provider, through: :good_provider
   has_many :good_providers, as: :good
   has_many :providers, through: :good_providers
-
   has_many :products
 
   has_one_attached :logo
-
+  
 end
--- app/models/facilitate_taxon.rb
+++ app/models/facilitate_taxon.rb
@@ -1,23 +1,11 @@
 class FacilitateTaxon < ApplicationRecord
-  acts_as_list
+  attribute :name, :string
+  attribute :position, :integer
+  attribute :facilitates_count, :integer, default: 0
 
   has_many :facilitates, dependent: :nullify
+  
   default_scope -> { order(position: :asc, id: :asc) }
-
+  
+  acts_as_list
 end
-
-
-# :name, :string, limit: 255
-# :type, :string, limit: 255
-# :price_reduce, integer, limit: 4
-# :start_at, :datetime
-# :finish_at, :datetime
-
-# 运费；
-# 运费计算，基于重量；
-#
-#
-# 保险；
-# 基于总价；
-#
-#
