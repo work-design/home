9d6d27add495a54136ec0059cc6515135ccd1eb4
qinmingyuan <mingyuan0715@foxmail.com>
2020-02-04 00:31:29 +0800

    fix compute logic

--- app/models/stat.rb
+++ app/models/stat.rb
@@ -8,10 +8,12 @@ class Stat < ApplicationRecord
 
   def compute(day = Date.today)
     self.date ||= day
-    self.oauth_users_count = OauthUser.default_where('created_at': date).count
-    self.subscribed_requests_count = SubscribeRequest.default_where('created_at': date).count
-    self.users_count = User.default_where('created_at': date).count
-    self.requirements_count = Requirement.default_where('updated_at': date).count
+    begin_time = date.beginning_of_day
+    end_time = (date + 1).beginning_of_day
+    self.oauth_users_count = OauthUser.default_where('created_at-gte': begin_time, 'created_at-lt': end_time).count
+    self.subscribed_requests_count = SubscribeRequest.default_where('created_at-gte': begin_time, 'created_at-lt': end_time).count
+    self.users_count = User.default_where('created_at-gte': begin_time, 'created_at-lt': end_time).count
+    self.requirements_count = Requirement.default_where('created_at-gte': begin_time, 'created_at-lt': end_time).count
     self
   end
 
