d3f7b0b8f7f71c739b5ea429b00c7022b6d443d7
qinmingyuan <mingyuan0715@foxmail.com>
2020-02-04 00:25:52 +0800

    add date

--- app/models/stat.rb
+++ app/models/stat.rb
@@ -1,11 +1,13 @@
 class Stat < ApplicationRecord
+  attribute :date, :date
   attribute :subscribed_requests_count, :integer
   attribute :oauth_users_count, :integer
   attribute :users_count, :integer
   attribute :requirements_count, :integer
 
 
-  def xx(date = Date.today)
+  def compute(day = Date.today)
+    self.date = day
     self.oauth_users_count = OauthUser.default_where('created_at': date).count
     self.subscribed_requests_count = SubscribeRequest.default_where('created_at': date).count
     self.users_count = User.default_where('created_at': date).count
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_03_162210) do
+ActiveRecord::Schema.define(version: 2020_02_03_162442) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -2062,6 +2062,7 @@ ActiveRecord::Schema.define(version: 2020_02_03_162210) do
     t.integer "requirements_count", scale: 4
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.date "date"
   end
 
   create_table "super_job_titles", force: :cascade do |t|
