0d6c7fd2d97f6607fe28758761d0abdabc84cbd2
qinmingyuan <mingyuan0715@foxmail.com>
2020-02-05 19:17:13 +0800

    set wxpay with cert

--- config/initializers/wx_pay.rb
+++ config/initializers/wx_pay.rb
@@ -1,8 +1,13 @@
 WxPay.appid = CREDENT.dig(:wechat, :appid)
 WxPay.key = CREDENT.dig(:wxpay, :key)
 WxPay.mch_id = CREDENT.dig(:wxpay, :mch_id)
-#
-# filepath = File.expand_path('../config/apiclient_cert.p12', __dir__)
-# WxPay.set_apiclient_by_pkcs12(File.read(filepath), WxPay.mch_id)
-#
-# WxPay.extra_rest_client_options = { timeout: 2, open_timeout: 3 }
+WxPay.extra_rest_client_options = {
+  timeout: 2,
+  open_timeout: 3
+}
+
+filepath = File.expand_path('../certs/apiclient_cert.p12', __dir__)
+if File.exist? filepath
+  WxPay.set_apiclient_by_pkcs12(File.read(filepath), WxPay.mch_id)
+end
+
