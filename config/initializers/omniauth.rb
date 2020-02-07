5b6bfb2faea87103f714c8f57681f05be7fa5cbd
qinmingyuan <mingyuan0715@foxmail.com>
2020-02-07 11:44:06 +0800

    upgrade search

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :developer unless Rails.env.production?
   provider :github, CREDENT.dig(:github, :key), CREDENT.dig(:github, :secret), scope: 'user,repo,gist'
-  provider :wechat, CREDENT.dig(:wechat, :appid), CREDENT.dig(:wechat, :secret), authorize_params: { scope: 'snsapi_base' }
+  #provider :wechat, CREDENT.dig(:wechat, :appid), CREDENT.dig(:wechat, :secret), authorize_params: { scope: 'snsapi_base' }
+  provider :wechat, CREDENT.dig(:dev_wechat, :appid), CREDENT.dig(:dev_wechat, :secret), authorize_params: { scope: 'snsapi_base' }
 end
