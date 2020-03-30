6f3a6eb6bb02071c11905c54d0d2cc89e444da4f
qinmingyuan <mingyuan0715@foxmail.com>
2020-03-30 21:17:18 +0800

    used choices js

--- app/javascript/packs/admin/application.js
+++ app/javascript/packs/admin/application.js
@@ -1,10 +1,12 @@
 import 'bulma_ext'
 import 'font_ext'
 import 'tooltip'
+import 'choices.js/src/styles/choices'
 
 import 'rails_com/application'
 import 'stimulus_com/all'
 import 'default_form/application'
 import 'moment'
+import 'choices.js'
 
 import '../channels'
