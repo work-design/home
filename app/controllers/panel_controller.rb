9d1d7ac6628ec803745d584f5b4f707c19b5fa99
qinmingyuan <mingyuan0715@foxmail.com>
2020-04-20 17:36:18 +0800

    fix rails role user

--- app/controllers/panel_controller.rb
+++ app/controllers/panel_controller.rb
@@ -3,4 +3,8 @@ class PanelController < ApplicationController
   before_action :require_role
   after_action :set_flash, only: [:update, :create, :destroy]
 
+  def rails_role_user
+    current_user
+  end
+
 end unless defined? PanelController
