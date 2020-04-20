677af84e815468aef74838145378f881493bc3dd
qinmingyuan <mingyuan0715@foxmail.com>
2020-04-20 17:32:49 +0800

    panel require role

--- app/controllers/panel_controller.rb
+++ app/controllers/panel_controller.rb
@@ -1,5 +1,6 @@
 class PanelController < ApplicationController
   before_action :require_login
+  before_action :require_role
   after_action :set_flash, only: [:update, :create, :destroy]
 
 end unless defined? PanelController
