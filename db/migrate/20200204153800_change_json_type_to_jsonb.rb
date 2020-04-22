1e548eca12ad9ceadd36c15ed37e7ae00a1ed6f3
qinmingyuan <mingyuan0715@foxmail.com>
2020-04-22 21:44:37 +0800

    moved some migrations

--- db/migrate/20200204153800_change_json_type_to_jsonb.rb
+++
@@ -1,6 +0,0 @@
-class ChangeJsonTypeToJsonb < ActiveRecord::Migration[6.0]
-  def change
-    change_column :parts, :part_taxon_ancestors, :jsonb
-    change_column :part_taxons, :parent_ancestors, :jsonb
-  end
-end
