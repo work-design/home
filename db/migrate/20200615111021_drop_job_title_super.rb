9d1056ebeac506a8455060ee2a731d6cda9a3c80
qinmingyuan <mingyuan0715@foxmail.com>
2020-06-24 16:06:49 +0800

    migration

--- db/migrate/20200615111021_drop_job_title_super.rb
+++
@@ -1,7 +0,0 @@
-class DropJobTitleSuper < ActiveRecord::Migration[6.0]
-  def change
-    drop_table :job_title_references
-    drop_table :member_supers
-    add_reference :job_titles, :super_job_title
-  end
-end
