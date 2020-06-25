d9db84cd5df64ee7f8cce045852bdaab74a9a9d3
qinmingyuan <mingyuan0715@foxmail.com>
2020-06-25 15:10:58 +0800

    support organ

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,7 @@ class ApplicationController < ActionController::Base
   include RailsRole::Application
   include RailsOrg::Application
   include RailsTrade::Application
+  before_action :support_organ
   protect_from_forgery with: :exception, unless: -> { json_format? }
   content_security_policy true
 
