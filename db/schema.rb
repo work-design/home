8fd39e4b3c8e0afff1e3a75664fdc713851ffb35
MingyuanQin <mingyuan0715@foxmail.com>
2020-10-21 21:30:09 +0800

    update gemfile

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_18_125822) do
+ActiveRecord::Schema.define(version: 2020_10_18_142602) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -2412,6 +2412,10 @@ ActiveRecord::Schema.define(version: 2020_10_18_125822) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "note"
+    t.decimal "fund_budget"
+    t.decimal "fund_expense"
+    t.decimal "budget_amount"
+    t.decimal "expense_amount"
   end
 
   create_table "project_taxons", id: { scale: 8 }, force: :cascade do |t|
